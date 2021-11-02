ec5e38a39f9c8e412e682427930be3777a3e76a0
jjimini98 <jjimini98@naver.com>
2021-11-02 23:22:10 +0900

    시각.py 진행중

---
+++ 2.Implementation/시각.py
@@ -0,0 +1,26 @@
+def solution(N):
+    # 00시 00분 00초 부터 N시 59분 59초까지의 모든 시각중 3이 하나라도 포함되는 모든 경우의  수 
+    # 3, 13, 23, 30, 31, 32, 33, 34, 35 , 36, 37, 38, 39, 43, 53
+    count = 0 
+    answer = [3, 13, 23, 30, 31, 32, 33, 34, 35 , 36, 37, 38, 39, 43, 53]
+    for hours in range(N+1):
+        for mins in range(0,60):
+            for secs in range(0,60):
+                if secs in answer:
+                    count+=1 
+                    continue
+            if mins in answer:
+                count+=1 
+                continue
+        if hours in answer:
+            count+=1
+            continue
+    return count
+
+
+
+
+if __name__ =="__main__":
+    # n= 5 --> 11475
+    N = int(input())
+    print(solution(N))
