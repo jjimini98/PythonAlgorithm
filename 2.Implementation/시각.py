241bc6cffa9be9e15bdb5011db8703b35b4ef077
jjimini98 <jjimini98@naver.com>
2021-11-03 10:38:19 +0900

    시각.py 완료

--- 2.Implementation/시각.py
+++ 2.Implementation/시각.py
@@ -1,20 +1,23 @@
 def solution(N):
     # 00시 00분 00초 부터 N시 59분 59초까지의 모든 시각중 3이 하나라도 포함되는 모든 경우의  수 
-    # 3, 13, 23, 30, 31, 32, 33, 34, 35 , 36, 37, 38, 39, 43, 53
     count = 0 
-    answer = [3, 13, 23, 30, 31, 32, 33, 34, 35 , 36, 37, 38, 39, 43, 53]
+    # answer = [3, 13, 23, 30, 31, 32, 33, 34, 35 , 36, 37, 38, 39, 43, 53]
     for hours in range(N+1):
         for mins in range(0,60):
             for secs in range(0,60):
-                if secs in answer:
-                    count+=1 
-                    continue
-            if mins in answer:
-                count+=1 
-                continue
-        if hours in answer:
-            count+=1
-            continue
+                if "3" in str(hours)+str(mins)+str(secs):
+                    print(str(hours)+str(mins)+str(secs))
+                    count+=1
+        # 시도는 좋았으나.. 정답이 아님. 왜 정답이 아닌건지?고민해보기 
+        #         if "3" in str(secs):
+        #             count +=1 
+        #             print("secs >> ",count)
+        #     if "3" in str(mins):               
+        #         count+=1
+        #         print("mins >> ",count)
+        # if "3" in str(hours):
+        #     count+=1
+        #     print("hours >> ",count)
     return count
 
 
