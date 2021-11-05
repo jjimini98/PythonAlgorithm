0c20267ef955c7506e0283fcb8965433e3039a8e
jjimini98 <jjimini98@naver.com>
2021-11-05 17:39:22 +0900

    스택/큐

---
+++ 3.DFS,BFS/stack.py
@@ -0,0 +1,14 @@
+# stack 코드로 구현하기
+
+stack = list()
+
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+print(stack) #5,2,3,1,4
