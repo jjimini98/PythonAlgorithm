cb41c3f9d3326a583f7ae7e8c6c7f9250cbe48a4
jjimini98 <jjimini98@naver.com>
2022-05-03 17:05:03 +0900

    0503 숫자문자열과 영단어 완료

---
+++ Programmers/숫자문자열과영단어.py
@@ -0,0 +1,16 @@
+# find 하고 있으면 새로운 문자열에 넣기 
+
+
+numbers = ["zero" ,"one","two" ,"three","four","five" , "six", "seven" ,"eight","nine"]
+
+def solution(s):
+    
+    for num in numbers:
+        if s.find(num) != -1:
+            s = s.replace(num, str(numbers.index(num)))
+    return s
+
+
+if __name__ == "__main__":
+    s = "23fourfour5six7"
+    print(solution(s))
