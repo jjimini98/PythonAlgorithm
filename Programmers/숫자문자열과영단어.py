52f179752c4d1e93f40df725b87ad3031a46c53b
jjimini98 <jjimini98@naver.com>
2022-05-03 17:07:39 +0900

    0504 다른사람 풀이 추가

--- Programmers/숫자문자열과영단어.py
+++ Programmers/숫자문자열과영단어.py
@@ -1,10 +1,9 @@
 # find 하고 있으면 새로운 문자열에 넣기 
 
 
-numbers = ["zero" ,"one","two" ,"three","four","five" , "six", "seven" ,"eight","nine"]
 
 def solution(s):
-    
+    numbers = ["zero" ,"one","two" ,"three","four","five" , "six", "seven" ,"eight","nine"]
     for num in numbers:
         if s.find(num) != -1:
             s = s.replace(num, str(numbers.index(num)))
@@ -14,3 +13,13 @@ def solution(s):
 if __name__ == "__main__":
     s = "23fourfour5six7"
     print(solution(s))
+
+
+# 다른사람 풀이
+# num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+
+# def solution(s):
+#     answer = s
+#     for key, value in num_dic.items():
+#         answer = answer.replace(key, value)
+#     return int(answer)
